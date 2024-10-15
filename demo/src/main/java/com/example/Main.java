f14637aac5907bd45fe6e9be143895a410d60314
lai2-24 <lai2-24@itismeucci.com>
2024-10-15 09:15:25 +0200

    ok

--- demo/src/main/java/com/example/Main.java
+++ demo/src/main/java/com/example/Main.java
@@ -3,29 +3,16 @@ package com.example;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.DataOutputStream;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        System.out.println("server avviato!");
-
-        ServerSocket s1 = new ServerSocket(3000); //La porta puo essere gia aperta quindi va gestita l'eccezione
-        Socket s = s1.accept();
-        System.out.println("un cliente si e creato");
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-
-        String stringaRicevuta = in.readLine();
-        System.out.println("La stringa ricevuta: " + stringaRicevuta);
-
-        String stringaMaiuscola = stringaRicevuta.toUpperCase();
-        out.writeBytes(stringaMaiuscola + '\n');
-
-        s.close();
-        s1.close();
+        System.out.println("Server Avviato!");
+        ServerSocket ss = new ServerSocket(3000);
+        do{
+            Socket s = ss.accept();
+            System.out.println("un client si è collegato");
+            MioThread t = new MioThread(s);
+            t.start();
+        }while(true);
     }
 }
---
+++ demo/src/main/java/com/example/MioThread.java
@@ -0,0 +1,40 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class MioThread extends Thread {
+    Socket s;
+
+    public MioThread(Socket s) {
+        this.s = s;
+    }
+
+    public void run() {
+        try {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+            do{
+                String stringaRicevuta = in.readLine();
+                if(stringaRicevuta.equals("!")){
+                    System.out.println("il client vuole chiudere");
+                    s.close();
+                    break;
+                }
+
+                System.out.println("Stringa ricevuta sul thread" + Thread.currentThread().getName() + ": " + stringaRicevuta);
+
+                String stringaTrasformata = stringaRicevuta.toUpperCase();
+                out.writeBytes(stringaTrasformata + "\n");
+            }while(true);
+
+        } catch (Exception e) {
+
+        }
+
+    }
+}
